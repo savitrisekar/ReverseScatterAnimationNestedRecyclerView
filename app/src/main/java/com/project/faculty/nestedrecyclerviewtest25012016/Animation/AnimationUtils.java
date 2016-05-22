2ab7bd869bc11cb5cb324017653e7881b9d6ac1c
Sreesha S Kuruvadi <kssreesha@gmail.com>
2016-05-22 18:14:53 +0530

    Refactoring and added ViewHolder.itemView pivot changes

--- app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/Animation/AnimationUtils.java
+++ app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/Animation/AnimationUtils.java
@@ -15,7 +15,7 @@ public class AnimationUtils {
     private static int counter = 0;
 
 
-    public static void animateScatter(RecyclerView.ViewHolder holder, boolean goesDown) {
+    public static void animateAlphaFadeAndScatter(RecyclerView.ViewHolder holder, boolean goesDown) {
         counter = ++counter % 4;
         int holderHeight = holder.itemView.getHeight();
         int holderWidth = holder.itemView.getWidth();
@@ -23,12 +23,17 @@ public class AnimationUtils {
         holderItemView.setPivotY(goesDown ? 0 : holderHeight);
         holderItemView.setPivotX(holderWidth / 2);
         AnimatorSet animatorSet = new AnimatorSet();
-        ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(holderItemView, "translationY", goesDown == true ? 300 : -300, 0);
-        ObjectAnimator animatorTranslateX = ObjectAnimator.ofFloat(holderItemView, "translationX", counter == 1 || counter == 3 ? holderWidth : -holderWidth, 0);
-        ObjectAnimator animatorScaleX = ObjectAnimator.ofFloat(holderItemView, "scaleX", counter == 1 || counter == 2 ? 0 : 2, 1f);
-        ObjectAnimator animatorScaleY = ObjectAnimator.ofFloat(holderItemView, "scaleY", counter == 1 || counter == 2 ? 0 : 2, 1f);
-        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(holderItemView, "alpha", 0f, 1f);
-        animatorAlpha.setInterpolator(new AccelerateInterpolator(1.5f));
+        ObjectAnimator animatorTranslateY
+                = ObjectAnimator.ofFloat(holderItemView, "translationY", goesDown == true ? 300 : -300, 0);
+        ObjectAnimator animatorTranslateX
+                = ObjectAnimator.ofFloat(holderItemView, "translationX", counter == 1 || counter == 3 ? holderWidth : -holderWidth, 0);
+        ObjectAnimator animatorScaleX
+                = ObjectAnimator.ofFloat(holderItemView, "scaleX", counter == 1 || counter == 2 ? 0 : 2, 1f);
+        ObjectAnimator animatorScaleY
+                = ObjectAnimator.ofFloat(holderItemView, "scaleY", counter == 1 || counter == 2 ? 0 : 2, 1f);
+        ObjectAnimator animatorAlpha
+                = ObjectAnimator.ofFloat(holderItemView, "alpha", 0f, 1f);
+        animatorAlpha.setInterpolator(new AccelerateInterpolator(5.5f));
         animatorSet.playTogether(animatorAlpha, animatorScaleX, animatorScaleY, animatorTranslateX, animatorTranslateY);
         animatorSet.setDuration(2000).setInterpolator(new DecelerateInterpolator(1.1f));
         animatorSet.start();
--- app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/NestedRecyclerViewActivity.java
+++ app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/NestedRecyclerViewActivity.java
@@ -1,18 +1,13 @@
 package com.project.faculty.nestedrecyclerviewtest25012016;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.project.faculty.nestedrecyclerviewtest25012016.Animation.AnimationUtils;
 
@@ -53,9 +48,9 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
             if (position > previousPosition) {
-                AnimationUtils.animateScatter(holder, true);
+                AnimationUtils.animateAlphaFadeAndScatter(holder, true);
             } else {
-                AnimationUtils.animateScatter(holder, false);
+                AnimationUtils.animateAlphaFadeAndScatter(holder, false);
             }
             previousPosition = position;
         }
@@ -94,9 +89,9 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
             @Override
             public void onBindViewHolder(ViewHolder holder, int position) {
                 if (position > prevPos) {
-                    AnimationUtils.animateScatter(holder, true);
+                    AnimationUtils.animateAlphaFadeAndScatter(holder, true);
                 } else {
-                    AnimationUtils.animateScatter(holder, false);
+                    AnimationUtils.animateAlphaFadeAndScatter(holder, false);
                 }
                 prevPos = position;
             }
