9263589c295608b32d614fe3957536e256a373dd
Sreesha S Kuruvadi <kssreesha@gmail.com>
2016-05-23 19:20:24 +0530

    Added Multiple Animations ! Expandable menu item contains animations !!

--- app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/Animation/AnimationUtils.java
+++ app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/Animation/AnimationUtils.java
@@ -8,12 +8,91 @@ import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 
+import com.project.faculty.nestedrecyclerviewtest25012016.R;
+
 /**
  * Created by Sreesha on 13-07-2015.
  */
 public class AnimationUtils {
+
     private static int counter = 0;
 
+    public static final String AVATAR_FLIP_SLIDE_IN_FADE_IN = "animateAvatarFadeInFlip";
+    public static final String SLIDE_LEFT_TO_RIGHT = "slideLeftToRight";
+    public static final String SLIDE_BOTTOM_TO_TOP = "slideBottomToTop";
+    public static final String ALPHA_FADE_SLIDE = "alphaFadeSlideIn";
+    public static final String FLIP = "flip";
+    public static final String REVERSE_SCATTER = "reverseScatter";
+
+    public static void animateAvatarFadeInFlip(RecyclerView.ViewHolder holder) {
+
+        View itemView = holder.itemView;
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator elementAnimatorTranslateX = ObjectAnimator.ofFloat(itemView, View.TRANSLATION_X, -900, 0);
+        AnimatorSet avatarAnimatorSet = new AnimatorSet();
+        ObjectAnimator avatarAnimatorFade = ObjectAnimator.ofFloat(
+                holder.itemView.findViewById(R.id.posterCard)
+                , View.ALPHA, 0, 1
+        );
+        ObjectAnimator avatarAnimatorFlipX = ObjectAnimator.ofFloat(holder.itemView.findViewById(R.id.posterCard)
+                , View.ROTATION_X, -70, 0, 70, 0);
+        avatarAnimatorSet.setDuration(500);
+        avatarAnimatorSet.playTogether(avatarAnimatorFade, avatarAnimatorFlipX);
+
+        animatorSet.setDuration(900);
+        animatorSet.setInterpolator(new DecelerateInterpolator(1.1f));
+        animatorSet.playSequentially(elementAnimatorTranslateX, avatarAnimatorSet);
+        animatorSet.start();
+    }
+
+    public static void slideLeftToRight(RecyclerView.ViewHolder holder) {
+
+        View view = holder.itemView;
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animatorTranslateX = ObjectAnimator.ofFloat(view, View.TRANSLATION_X, -900, 0);
+        animatorSet.setDuration(900);
+        animatorSet.setInterpolator(new DecelerateInterpolator(1.1f));
+        animatorSet.playTogether(animatorTranslateX);
+        animatorSet.start();
+    }
+
+    public static void slideBottomToTop(RecyclerView.ViewHolder holder, boolean goesDown) {
+        View view = holder.itemView;
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, goesDown? 700 : -700, 0);
+        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(view, View.ALPHA, 1f, 1f);
+
+        animatorSet.setDuration(900);
+        animatorSet.setInterpolator(new DecelerateInterpolator(1.1f));
+        animatorSet.playTogether(animatorTranslateY, animatorAlpha);
+        animatorSet.start();
+    }
+
+    public static void alphaFadeSlide(RecyclerView.ViewHolder holder) {
+        counter++;
+        View view = holder.itemView;
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animatorTranslateX = ObjectAnimator.ofFloat(view, View.TRANSLATION_X, counter % 2 == 0 ? 400 : -400, 0);
+        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(view, View.ALPHA, 0, 1f);
+        animatorSet.setDuration(2000);
+        animatorSet.setInterpolator(new DecelerateInterpolator(1.1f));
+        animatorSet.playTogether(animatorTranslateX, animatorAlpha);
+        animatorSet.start();
+    }
+
+    public static void viewFlip(RecyclerView.ViewHolder holder) {
+        counter++;
+        View view = holder.itemView;
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animatorTranslateX = ObjectAnimator.ofFloat(view, View.ROTATION_X, counter % 2 == 0 ? 100 : -100, 0);
+        ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(view, View.ROTATION_Y, -10, 0);
+
+        animatorSet.setInterpolator(new DecelerateInterpolator(1.1f));
+        animatorSet.setDuration(2000);
+        animatorSet.playTogether(animatorTranslateX, animatorTranslateY);
+        animatorSet.start();
+    }
 
     public static void animateAlphaFadeAndScatter(RecyclerView.ViewHolder holder, boolean goesDown) {
         counter = ++counter % 4;
--- app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/NestedRecyclerViewActivity.java
+++ app/src/main/java/com/project/faculty/nestedrecyclerviewtest25012016/NestedRecyclerViewActivity.java
@@ -1,24 +1,39 @@
 package com.project.faculty.nestedrecyclerviewtest25012016;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.project.faculty.nestedrecyclerviewtest25012016.Animation.AnimationUtils;
 
 public class NestedRecyclerViewActivity extends AppCompatActivity {
+
     RecyclerView outerRecyclerView;
     OuterRecyclerViewCA outerRVAdapter;
     private int previousPosition = 0;
+    private String mAnimationType = AnimationUtils.REVERSE_SCATTER;
+    private final String ANIMATION_TYPE_STRING_KEY = "animationTypeStringKey";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nested_recycler_view);
+
+        if (savedInstanceState != null) {
+            mAnimationType = savedInstanceState.getString(ANIMATION_TYPE_STRING_KEY,AnimationUtils.REVERSE_SCATTER);
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -30,11 +45,20 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
         );
         outerRecyclerView.setAdapter(outerRVAdapter);
     }
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+
+        outState.putString(ANIMATION_TYPE_STRING_KEY, mAnimationType);
+        outPersistentState.putString(ANIMATION_TYPE_STRING_KEY, mAnimationType);
+
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
 
     public class OuterRecyclerViewCA extends RecyclerView.Adapter<OuterRecyclerViewCA.ViewHolder> {
         InnerRecyclerViewCA innerRCAdapter;
 
         OuterRecyclerViewCA() {
+
             innerRCAdapter = new InnerRecyclerViewCA();
         }
 
@@ -47,12 +71,45 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-            if (position > previousPosition) {
-                AnimationUtils.animateAlphaFadeAndScatter(holder, true);
-            } else {
-                AnimationUtils.animateAlphaFadeAndScatter(holder, false);
+        }
+
+        @Override
+        public void onViewAttachedToWindow(ViewHolder holder) {
+            super.onViewAttachedToWindow(holder);
+            switch (mAnimationType) {
+                case AnimationUtils.REVERSE_SCATTER:
+
+                    if (holder.getAdapterPosition() > previousPosition) {
+                        AnimationUtils.animateAlphaFadeAndScatter(holder, true);
+                    } else {
+                        AnimationUtils.animateAlphaFadeAndScatter(holder, false);
+                    }
+                    previousPosition = holder.getAdapterPosition();
+
+                    break;
+
+                case AnimationUtils.FLIP:
+                    AnimationUtils.viewFlip(holder);
+                    break;
+                case AnimationUtils.SLIDE_BOTTOM_TO_TOP:
+                    if (holder.getAdapterPosition() > previousPosition) {
+                        AnimationUtils.slideBottomToTop(holder, true);
+                    } else {
+                        AnimationUtils.slideBottomToTop(holder, true);
+                    }
+                    previousPosition = holder.getAdapterPosition();
+
+                    break;
+                case AnimationUtils.SLIDE_LEFT_TO_RIGHT:
+                    AnimationUtils.slideLeftToRight(holder);
+                    break;
+                case AnimationUtils.AVATAR_FLIP_SLIDE_IN_FADE_IN:
+                    AnimationUtils.animateAvatarFadeInFlip(holder);
+                    break;
+                case AnimationUtils.ALPHA_FADE_SLIDE:
+                    AnimationUtils.alphaFadeSlide(holder);
+                    break;
             }
-            previousPosition = position;
         }
 
         @Override
@@ -62,6 +119,7 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
 
         class ViewHolder extends RecyclerView.ViewHolder {
             RecyclerView innerRecyclerView;
+            CardView posterCard;
 
             public ViewHolder(View itemView) {
                 super(itemView);
@@ -70,6 +128,7 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
                         new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false)
                 );
                 innerRecyclerView.setAdapter(innerRCAdapter);
+                posterCard = (CardView) itemView.findViewById(R.id.posterCard);
             }
 
             public RecyclerView getInnerRecyclerView() {
@@ -78,10 +137,11 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
         }
 
         public class InnerRecyclerViewCA extends RecyclerView.Adapter<InnerRecyclerViewCA.ViewHolder> {
-            private int prevPos=0;
+            private int prevPos = 0;
+
             @Override
             public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                View view =  LayoutInflater.from(parent.getContext())
+                View view = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.inner_recycler_view_row_element, parent, false);
                 return new ViewHolder(view);
             }
@@ -108,5 +168,60 @@ public class NestedRecyclerViewActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_nested_recycler_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.animate_avatar_fade_in_flip:
+                mAnimationType = AnimationUtils.AVATAR_FLIP_SLIDE_IN_FADE_IN;
+                if (outerRecyclerView != null) {
+                    outerRecyclerView.invalidate();
+                }
+                break;
+            case R.id.alpha_fade_slide_in:
+                mAnimationType = AnimationUtils.ALPHA_FADE_SLIDE;
+                if (outerRecyclerView != null) {
+                    outerRecyclerView.invalidate();
+                }
+                break;
+            case R.id.slide_left_to_right:
+                mAnimationType = AnimationUtils.SLIDE_LEFT_TO_RIGHT;
+                if (outerRecyclerView != null) {
+                    outerRecyclerView.invalidate();
+                }
+                break;
+            case R.id.slide_bottom_to_top:
+                mAnimationType = AnimationUtils.SLIDE_BOTTOM_TO_TOP;
+                if (outerRecyclerView != null) {
+                    outerRecyclerView.invalidate();
+                }
+                break;
+            case R.id.reverse_scatter:
+                mAnimationType = AnimationUtils.REVERSE_SCATTER;
+                if (outerRecyclerView != null) {
+                    outerRecyclerView.invalidate();
+                }
+                break;
+            case R.id.flip:
+                mAnimationType = AnimationUtils.FLIP;
+                if (outerRecyclerView != null) {
+                    outerRecyclerView.invalidate();
+                }
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
